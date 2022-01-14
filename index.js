3189ee6d4865a834ece8ed9962cb17c463661bf5
sloth-hub <madamhh@gmail.com>
2022-01-14 22:32:24 +0900

    Fix item tab and item list, and fix it can add items to canvas screen

--- index.js
+++ index.js
@@ -1,120 +1,94 @@
 const container = document.querySelector(".container");
 const canvas = new fabric.Canvas("display", { width: 648, height: container.offsetHeight });
+canvas.selection = false;
 const start = document.querySelector(".start-btn");
+let itemTab = document.createElement("div");
+const itemsWrap = document.createElement("div");
+let itemList;
+let toggleTab;
+let itemData;
 
 init();
 
 function init() {
 
-    const roomImg = "./images/room.png";
-
     start.addEventListener("click", async ({ target }) => {
         if (target.className === "start-btn") {
-            container.removeChild(target);
-            canvas.setBackgroundImage(roomImg, canvas.renderAll.bind(canvas), {
-                left: (canvas.width / 2 - 600 / 2),
-                top: (canvas.height / 2 - 600 / 1.9)
-            });
-            const itemsWrap = document.createElement("div");
+            const roomImg = "./images/room.png";
             itemsWrap.className = "items-wrap";
+            itemTab.className = "tab-toggle";
+            itemTab.innerText = "▼";
+            container.removeChild(target);
+            container.appendChild(itemTab);
             container.appendChild(itemsWrap);
+
             itemsWrap.innerHTML = `
-            <div class="tab-toggle">▼</div>
             <ul class="item-tab"></ul>
             <ul class="item-list"></ul>`;
 
-            const itemData = await axios.get("test.json")
+            itemData = await axios.get("test.json")
                 .then(({ data }) => data)
                 .catch(error => console.log(error));
 
-            const itemTab = document.querySelector(".item-tab");
-            const itemList = document.querySelector(".item-list");
+            itemTab = document.querySelector(".item-tab");
+            itemList = document.querySelector(".item-list");
+            toggleTab = document.querySelector(".tab-toggle");
             for (let key in itemData) {
                 itemTab.innerHTML += `<li>${key}</li>`;
             }
             itemList.innerHTML = `<p class="item-list-msg">Click on the tab!</p>`;
-            clickedTab(itemTab, itemList, itemData);
+
+            canvas.setBackgroundImage(roomImg, canvas.renderAll.bind(canvas), {
+                left: (canvas.width / 2 - 600 / 2),
+                top: (canvas.height / 2 - 600 / 1.9)
+            });
+
+            menuInit();
         }
     });
 }
 
-function clickedTab(itemTab, itemList, itemData) {
-    const tabToggle = document.querySelector(".tab-toggle");;
+function menuInit() {
 
-    tabToggle.addEventListener("click", () => {
-        itemList.classList.toggle("hide");
-        itemTab.classList.toggle("hide");
-        if (tabToggle.innerText === "▼") {
-            tabToggle.innerText = "▲";
-        } else {
-            tabToggle.innerText = "▼";
-        }
-    });
-
-    itemTab.addEventListener("click", ({ target }) => {
-
-        let categoryName = target.innerText;
-        itemList.innerHTML = "";
-        itemData[categoryName].forEach((e) => {
-            let template = itemListTemp({ categoryName, category: e });
-            itemList.innerHTML += template;
-        });
-        clickedItemList(itemList, categoryName);
-
-    });
+    toggleTab.addEventListener("click", tabToggle);
+    itemTab.addEventListener("click", clickedTab);
+    itemList.addEventListener("click", clickedList);
 }
 
-function clickedItemList(itemList, categoryName) {
+function tabToggle({ target }) {
 
-    itemList.addEventListener("click", ({ target }) => {
-        if (target.classList.contains(categoryName)) {
-            display.style.backgroundColor = target.style.backgroundColor;
-        } else if (target.className === "item-img") {
-            item.src = target.src;
-            x = 10;
-            y = 10;
-            WIDTH = item.width;
-            HEIGHT = item.height;
-            draw(item);
-            onClickedItem();
-        }
-    });
-}
-
-function draw(item) {
-    ctx.drawImage(item, x, y);
-}
-
-function clear() {
-}
-
-function drag(e) {
-    if (isDragging) {
-        x = e.clientX - canvas.offsetLeft;
-        y = e.clientY - canvas.offsetTop;
-        draw(item);
+    if (target.innerText === "▼") {
+        target.innerText = "▲";
+        itemsWrap.classList.toggle("hide");
+    } else {
+        target.innerText = "▼";
+        itemsWrap.classList.toggle("hide");
     }
 }
 
-function dragStart(e) {
-    isDragging = true;
-    x = e.clientX - canvas.offsetLeft;
-    y = e.clientY - canvas.offsetTop;
+function clickedTab({ target }) {
+
+    itemList.innerHTML = "";
+    itemData[target.innerText].forEach((e) => {
+        let template = itemListTemp({
+            categoryName: target.innerText,
+            category: e
+        });
+        itemList.innerHTML += template;
+    });
+
 }
 
-function dragEnd() {
-    isDragging = false;
-}
-
-function onClickedItem() {
-
-    canvas.addEventListener("touchstart", dragStart, { passive: false });
-    canvas.addEventListener("touched", dragEnd, { passive: false });
-    canvas.addEventListener("touchmove", drag, { passive: false });
-
-    canvas.addEventListener("mousedown", dragStart, { passive: false });
-    canvas.addEventListener("mouseup", dragEnd, { passive: false });
-    canvas.addEventListener("mousemove", drag, { passive: false });
+function clickedList({ target }) {
+    if (target.classList.contains("background")) {
+        display.style.backgroundColor = target.style.backgroundColor;
+    } else if (target.className === "item-img") {
+        fabric.Image.fromURL(target.src, (img) => {
+            canvas.add(img);
+        });
+        fabric.Object.prototype.hasControls = false;
+        fabric.Object.prototype.hasBorders = false;
+    }
 }
 
 function download(target) {
